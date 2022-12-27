68232cd57369a400d7b79152a1bc409d2b03f70f
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2022-12-27 14:15:38 +0300

    Slug attribute added to CourseSchema

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
@@ -14,12 +15,24 @@ const CourseSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        required: true,
-    },
-    image: {
-        type: String,
         default: Date.now,
     },
+    // image: {
+    //     type: String,
+    //     default: Date.now,
+    // },
+    slug: {
+        type: String,
+        unique: true,
+    },
+});
+
+CourseSchema.pre('validate', function (next) {
+    this.slug = slugify(this.name, {
+        lower: true,
+        strict: true,
+    });
+    next();
 });
 
 const Course = mongoose.model('Course', CourseSchema);
