dbb2a7d24d3485d38fd2b83e94021ba6d3dadab8
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2022-12-27 14:04:00 +0300

    Routing for course.ejs implemented

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ app.set('view engine', 'ejs');
 //MIDDLEWARE
 app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //ROUTES
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/course', courseRoute);
 
 const port = 3000;
 
