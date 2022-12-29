5b9eb2454c0f6ea6562cda3153dfc8b184eee6ad
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2022-12-29 22:44:04 +0300

    User login and logoff functionality added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -16,13 +18,30 @@ mongoose.connect('mongodb://127.0.0.1/smartedu-db').then(() => {
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
+//GLOBAL VARIABLE
+
+global.userIN = null;
+
 //MIDDLEWARE
 app.use(express.static('public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: 'my_keyboard_cat',
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: 'mongodb://127.0.0.1/smartedu-db',
+        }),
+    })
+);
 
 //ROUTES
+app.use('*', (req, res, next) => {
+    userIN = req.session.userID;
+    next();
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/course', courseRoute);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,40 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
     try {
         const user = await User.create(req.body);
 
-        res.status(201).redirect('/');
+        res.status(201).redirect('/login');
     } catch (error) {
         res.status(400).json({
             status: 'fail',
             error,
         });
     }
+};
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        const same = await bcrypt.compare(password, user.password);
+        req.session.userID = user._id;
+        if (same) {
+            res.status(200).redirect('/');
+        } else {
+            res.send('Password does not match email');
+        }
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+};
+
+exports.logoutUser = (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
 };
