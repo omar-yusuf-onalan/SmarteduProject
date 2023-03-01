fd5774ac61da83f045ba47db4ac9e3648bc7f7d4
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2023-03-01 10:24:55 +0300

    Course enrolling and dropping for students added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,9 +1,15 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const User = require('../models/User')
 
 exports.createCourse = async (req, res) => {
     try {
-        const course = await Course.create(req.body);
+        const course = await Course.create({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            user: req.session.userID,
+        });
         res.status(201).redirect('/courses')
         console.log(course);
     } catch (error) {
@@ -18,6 +24,8 @@ exports.getAllCourses = async (req, res) => {
     try {
         const categorySlug = req.query.categories;
 
+        const query = req.query.search;
+
         const category = await Category.findOne({ slug: categorySlug });
 
         let filter = {};
@@ -26,7 +34,21 @@ exports.getAllCourses = async (req, res) => {
             filter = { category: category._id };
         }
 
-        const courses = await Course.find(filter).sort('-createdAt');
+        if(query) {
+            filter = {name:query}
+        }
+    
+        if(!query && !categorySlug) {
+            filter.name = "",
+            filter.category = null
+        }
+
+        const courses = await Course.find({
+            $or:[
+                {name: { $regex: '.*' + filter.name + '.*', $options: 'i'}},
+                {category: filter.category}
+              ]
+        }).sort('-createdAt').populate('user');
 
         const categories = await Category.find();
 
@@ -45,10 +67,15 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
     try {
-        const course = await Course.findOne({ slug: req.params.slug });
+        const course = await Course.findOne({ slug: req.params.slug }).populate('user');
+        const user = await User.findById(req.session.userID)
+        const categories = await Category.find();
+
         res.status(200).render('course', {
             course,
             page_name: 'course',
+            user,
+            categories,
         });
     } catch (error) {
         res.status(400).json({
@@ -56,4 +83,35 @@ exports.getCourse = async (req, res) => {
             error,
         });
     }
+};
+
+exports.enrollCourse = async (req, res) => {
+    try {
+  
+      const user = await User.findById(req.session.userID);
+      await user.courses.addToSet({_id: req.body.course_id});
+      await user.save();
+  
+      res.status(200).redirect('/users/dashboard');
+    } catch (error) {
+      res.status(400).json({
+        status: 'fail',
+        error,
+      });
+    }
+  };
+
+exports.dropCourse = async (req, res) => {
+try {    
+    const user = await User.findById(req.session.userID);
+    await user.courses.pull({_id:req.body.course_id});
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
+} catch (error) {
+    res.status(400).json({
+    status: 'fail',
+    error,
+    });
+}
 };
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.route('/').post(roleMiddleware(['teacher', 'admin']), courseController.createCourse);
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
+router.route('/enroll').post(courseController.enrollCourse);
+router.route('/drop').post(courseController.dropCourse);
 
 module.exports = router;
