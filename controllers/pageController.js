ba77e3cded66f2e4ad166d903930834e4ff6d6b0
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2023-03-02 12:39:35 +0300

    Flash messages added to contact.ejs and courses.ejs

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -33,39 +33,46 @@ exports.getRegisterPage = (req, res) => {
 
 exports.sendEmail = async (req, res) => {
 
-const outputMessage = `
+try { 
+    const outputMessage = `
 
-<h1>Mail Details </h1>
-<ul>
-    <li>Name: ${req.body.name}</li>
-    <li>Email: ${req.body.email}</li>
-</ul>
-<h1>Message</h1>
-<p>${req.body.message}</p>
-`
+    <h1>Mail Details </h1>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-    user: "xyz@gmail.com", // gmail account
-    pass: "pass123", // gmail password
-    },
-});
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+        user: "xyz@gmail.com", // gmail account
+        pass: "pass123", // gmail password
+        },
+    });
 
-// send mail with defined transport object
-let info = await transporter.sendMail({
-    from: '"Smart EDU Contact Form" <arinyazilim@gmail.com>', // sender address
-    to: "gcekic@gmail.com", // list of receivers
-    subject: "Smart EDU Contact Form New Message ✔", // Subject line
-    html: outputMessage, // html body
-});
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Smart EDU Contact Form" <arinyazilim@gmail.com>', // sender address
+        to: "gcekic@gmail.com", // list of receivers
+        subject: "Smart EDU Contact Form New Message ✔", // Subject line
+        html: outputMessage, // html body
+    });
 
-console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", info.messageId);
 
-console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-res.status(200).redirect('contact');
+    req.flash('success', 'We received your message successfully')
 
+    res.status(200).redirect('contact');
+
+} catch (err) {
+    req.flash("error", `Something happened!`);
+    res.status(200).redirect('contact');
+}
 };
