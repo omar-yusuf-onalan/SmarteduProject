fdaea6f40cfd8a87a882bba3311551a713637d0b
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2023-03-02 18:21:34 +0300

    updateCourse route added

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -8,6 +8,7 @@ router.route('/').post(roleMiddleware(['teacher', 'admin']), courseController.cr
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 router.route('/:slug').delete(courseController.deleteCourse);
+router.route('/:slug').post(courseController.updateCourse);
 router.route('/enroll').post(courseController.enrollCourse);
 router.route('/drop').post(courseController.dropCourse);
 
