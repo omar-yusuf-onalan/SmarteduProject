96653f63e43163d1dd2ff6f1d4bdcb67383ecf1e
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2023-03-02 15:01:36 +0300

    deleteCourse function added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -20,6 +20,23 @@ exports.createCourse = async (req, res) => {
     }
 };
 
+exports.deleteCourse = async (req, res) => {
+    try {    
+  
+      const course = await Course.findOneAndRemove({slug:req.params.slug})
+  
+      req.flash("error", `${course.name} has been removed successfully`);
+  
+      res.status(200).redirect('/users/dashboard');
+  
+    } catch (error) {
+      res.status(400).json({
+        status: 'fail',
+        error,
+      });
+    }
+  };
+
 exports.getAllCourses = async (req, res) => {
     try {
         const categorySlug = req.query.categories;
