35ab734eeee23dbdbbc626fd8ef98798ec10ad21
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2023-03-02 14:56:33 +0300

    Method-override middleware added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const flash = require('connect-flash')
+const flash = require('connect-flash');
+const methodOverride = require('method-override')
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -42,6 +43,12 @@ app.use((req, res, next)=> {
     res.locals.flashMessages = req.flash();
     next();
   })
+
+app.use(
+methodOverride('_method', {
+    methods: ['POST', 'GET'],
+})
+);
 
 //ROUTES
 app.use('*', (req, res, next) => {
